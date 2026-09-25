b67d197bdf86495906fc440479fc0d078d225b18
Latheesh93-sys <Latheeshkkm@gmail.com>
2026-09-25 16:41:25 +0530

    environment corrected

--- CodeLatheesh/src/environments/environment.ts
+++ CodeLatheesh/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    production: false,
-    apiBaseUrl:'https://localhost:7004'
+    apiBaseUrl:'https://personalfinancetracker-fjewfye4g7fjdqcn.centralindia-01.azurewebsites.net'
 };
